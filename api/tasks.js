31039d7acb1ef4424cef175f08843376d2623305
MuhamadOmr <mohamadomr33@gmail.com>
2017-08-08 20:43:01 +0200

    adding two endpoint /update /delete

--- api/tasks.js
+++ api/tasks.js
@@ -21,20 +21,18 @@ var tasks = function (){
           }
         }).then(function(response){
 
-      res.send(JSON.stringify(response.data, null, 2));
+      res.send(response.data);
      return next();
     })
 
 }
 
-  function updateDevices(req, res, next) {
+  function updateDevice(req, res, next) {
 
-    var data = req.body;
 
     var ID = req.params.deviceId;
-    axios.put(
+    axios.get(
         'http://localhost:4000/2/subscriptions/'+ ID,
-        data,
         {
           headers: {
             'Content-Type': 'application/json',
@@ -43,18 +41,56 @@ var tasks = function (){
           }
         }).then(function(response){
 
-      res.send(JSON.stringify(response.data, null, 2));
+      res.send(response.data);
       return next();
     })
-
-     // res.send(JSON.stringify(params, null, 2));
-
   }
 
+
+  function getDevice(req, res, next) {
+
+
+    var ID = req.params.deviceId;
+    axios.get(
+        'http://localhost:4000/2/subscriptions/'+ ID,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'X-PUSHBOTS-APPID':'587772f5357e7195020041a7',
+            'X-PUSHBOTS-SECRET':'b0ae0b20d89f819a44280b88c1aead34'
+          }
+        }).then(function(response){
+
+      res.send(response.data);
+      return next();
+    })
+  }
+
+  function deleteDevice(req, res, next) {
+
+
+    var ID = req.params.deviceId;
+   // console.log(ID);
+    axios.delete(
+        'http://localhost:4000/2/subscriptions/'+ ID,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'X-PUSHBOTS-APPID':'587772f5357e7195020041a7',
+            'X-PUSHBOTS-SECRET':'b0ae0b20d89f819a44280b88c1aead34'
+          }
+        }).then(function(response){
+
+      res.send(response.data);
+      return next();
+    })
+  }
 // public code
 return {
  registerDevices: registerDevices,
-  updateDevices: updateDevices,
+  updateDevice:   updateDevice,
+  getDevice:      getDevice,
+  deleteDevice:   deleteDevice,
 
 }
 
--- server.js
+++ server.js
@@ -15,8 +15,11 @@ server.use(restify.plugins.bodyParser());
 
 server.post('/register-device', tasks.registerDevices);
 
-server.post('/update-device/:deviceId', tasks.updateDevices);
+server.post('/update-device/:deviceId', tasks.updateDevice);
 
+server.get('/get-device/:deviceId', tasks.getDevice);
+
+server.del('/delete-device/:deviceId', tasks.deleteDevice);
 
 
 server.listen(8088, function() {
